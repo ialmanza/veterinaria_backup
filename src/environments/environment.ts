d913d0c5bedeecf5fae14190f2cb4d33a557482c
ialmanza <isra.almanzagonzalez@gmail.com>
2024-12-06 11:51:02 -0300

    Listo con tabla de administracion de roles y el codigo limpio

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://fdqcganrmqgepkxgkugn.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkcWNnYW5ybXFnZXBreGdrdWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ4NzgyMTIsImV4cCI6MjAzMDQ1NDIxMn0.Q77MmGHtF3JzebU8Z05bWYVO4uGhxoYwIxPnxUQY870',
-};
